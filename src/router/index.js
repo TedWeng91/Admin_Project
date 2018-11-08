63d9e11c6c0c2fd6a82e758680601db5334d6d87
Ted <tedweng@gmail.com>
2018-11-08 21:56:57 +0800

    登陆页面完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Logic from '../views/Logic.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/logic',
+      name: 'logic',
+      component: Logic
     }
   ]
 })
