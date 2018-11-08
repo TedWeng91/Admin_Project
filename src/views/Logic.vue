772ca7c0fc626835e6bb316bcbe7cf10ded6592f
Ted <tedweng@gmail.com>
2018-11-08 22:01:45 +0800

    登陆页面完成2

--- src/views/Logic.vue
+++ src/views/Logic.vue
@@ -25,11 +25,20 @@ export default {
       form: {
         username: '',
         password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
       }
     }
   }
 }
 </script>
+
 <style scoped lang="scss">
 .login {
   position: fixed;
